28ca1d6c14922c99d9d99f8a2621131ad8326164
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 02:20:04 +0530

    Files generated: App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import DashboardPage from "./pages/DashboardPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import LoginPage from "./pages/LoginPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import SignUpPage from "./pages/SignUpPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+
+const App = () => (
+<QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+        <Routes>
+
+
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          {/* catch-all */}
+          <Route path="*" element={<NotFound />} />
+
+
+        </Routes>
+    </BrowserRouter>
+    </TooltipProvider>
+</QueryClientProvider>
+);
+
+export default App;
